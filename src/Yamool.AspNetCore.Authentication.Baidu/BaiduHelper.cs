5f8fde5532cbd3a12f5c90f4e6ce1b834fad1d92
zhengchun <zhengchunster@gmail.com>
2017-02-20 10:23:32 +0800

    Baidu

---
+++ src/Yamool.AspNetCore.Authentication.Baidu/BaiduHelper.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Yamool.AspNetCore.Authentication.Taobao
+{
+    public class BaiduHelper
+    {
+        public static string GetId(JObject user) => user.Value<string>("uid");
+
+        public static string GetName(JObject user) => user.Value<string>("uname");
+
+        public static string GetPortrait(JObject user) => user.Value<string>("portrait");       
+    }
+}
